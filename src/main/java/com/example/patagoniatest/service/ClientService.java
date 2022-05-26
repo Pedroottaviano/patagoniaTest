d42004d751c0ed4c5d9697d1b03c57564e3962a7
pedro-ottaviano-globant <pedro.ottaviano@globant.com>
2022-05-26 11:54:15 -0300

    first commit

---
+++ src/main/java/com/example/patagoniatest/service/ClientService.java
@@ -0,0 +1,35 @@
+package com.example.patagoniatest.service;
+
+import com.example.patagoniatest.model.Client;
+import com.example.patagoniatest.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public List<Client> getClients() {
+        return clientRepository.findAll();
+    }
+
+    public Client addClient(Client client) {
+        return clientRepository.save(client);
+    }
+
+    public void deleteClient(Long id) {
+        clientRepository.deleteById(id);
+    }
+
+}
